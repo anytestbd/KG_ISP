e3a1a92c336f4fb817dbca48910d10f79ee3ca17
anytestbd <anytest.bd@gmail.com>
2025-12-26 02:45:35 +0600

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,21 @@
+<script type="module">
+  import { initializeApp } from
+    "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+  import { getAuth } from
+    "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+  import { getFirestore } from
+    "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+  const firebaseConfig = {
+    apiKey: "আপনার apiKey",
+    authDomain: "আপনার authDomain",
+    projectId: "আপনার projectId",
+    storageBucket: "আপনার storageBucket",
+    messagingSenderId: "আপনার messagingSenderId",
+    appId: "আপনার appId"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  window.auth = getAuth(app);
+  window.db = getFirestore(app);
+</script>
